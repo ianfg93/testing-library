e5c9370bd5c1bd51598caf3cd3e24449578ed289
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-09-20 15:34:56 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+  renderWithRouter(<NotFound />);
+  const headingH2 = screen.getByRole('heading', {
+    name: 'Page requested not found', level: 2 });
+  const image = screen.getByRole('img');
+
+  expect(headingH2).toBeInTheDocument();
+  expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
