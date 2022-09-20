d51b1be253bcea6be63d9c04e3310778b9ae318f
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-09-20 15:30:03 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const noFavorite = screen.getByText('No favorite pokemon found');
+
+  expect(noFavorite).toBeInTheDocument();
+});
